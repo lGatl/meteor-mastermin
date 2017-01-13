8447ff9731328c95484231da4a369a0653e4ef06
lGatl <gat55@live.fr>
2017-01-13 18:54:52 +0100

    1er

---
+++ client/main.js
@@ -0,0 +1,18 @@
+
+ let body=document.getElementsByTagName("body")[0];
+ console.log('body', body);
+let bd = body.childNodes;
+
+document.getElementsByTagName("body")[0]
+
+ pions = body.geadd.tElementsByClassName("bon");
+
+console.log(pions);
+console.log(pions[0]);
+for(p in pions){
+	p.innerHTML="ezrezfgfrerez"
+}
+
+let yop = document.querySelectorAll("div.pion");
+console.log(yop);
+console.log(yop[0]);
